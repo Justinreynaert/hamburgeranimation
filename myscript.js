9e200d923f0986fbbe7ba8a67fe43b9acc4befca
Justin Reynaert <Justin@MacBook-Pro-van-admin.local>
2017-06-20 15:51:46 +0200

    Message

--- myscript.js
+++ myscript.js
@@ -4,6 +4,10 @@ window.onload = function() {
   let hamburger = document.getElementById("hamburger");
   let menuList = document.getElementById("menu");
 
+  let messages = document.getElementById("messages");
+  let baraterMenu = document.getElementById("barater");
+
+  let overlay = document.getElementById("overlay");
 
   //Listener
   hamburger.addEventListener("click", function() {
@@ -15,6 +19,25 @@ window.onload = function() {
 
 
   });
+
+  messages.addEventListener("click", function() {
+
+    baraterMenu.classList.toggle("show");
+    overlay.classList.toggle("show");
+    document.body.classList.toggle("noscroll");
+
+
+    if (overlay.classList.contains("show")) {
+      this.classList.remove("show");
+    }
+  })
+
+  overlay.addEventListener("click", function() {
+    baraterMenu.classList.toggle("show");
+    overlay.classList.remove("show");
+    document.body.classList.toggle("noscroll");
+
+  })
 
 
 }
